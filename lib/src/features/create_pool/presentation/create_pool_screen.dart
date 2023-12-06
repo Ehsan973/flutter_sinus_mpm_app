9235fbf79ddf33bdf9a40d629d0de2a0eca9c120
Ali kheiri <kheiria313@gmail.com>
2023-12-06 12:54:15 +0100

    add features to project .

---
+++ lib/src/features/create_pool/presentation/create_pool_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CreatePoolScreen extends StatelessWidget {
+  const CreatePoolScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(children: [
+        Text('Create pool screen ')
+      ]),
+    );
+  }
+}
---
+++ lib/src/features/join_pool/presentation/join_pool_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class JoinPoolScreen extends StatelessWidget {
+  const JoinPoolScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(children: [
+        Text('Join pool screen')
+      ]),
+    )
+    ; 
+  }
+}
