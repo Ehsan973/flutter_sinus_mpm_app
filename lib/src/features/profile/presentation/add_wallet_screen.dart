ad3fed7706c2d138c7d72d50f6903d9d49a7377f
bahador96 <bahador.foladi2@gmail.com>
2023-12-06 22:09:47 +0330

    add item random

--- lib/src/features/profile/presentation/add_wallet_screen.dart
+++ lib/src/features/profile/presentation/add_wallet_screen.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
 
-class AddWalletScreen extends StatelessWidget {
+class AddWalletScreen extends StatefulWidget {
   const AddWalletScreen({super.key});
+
+  @override
+  State<AddWalletScreen> createState() => _AddWalletScreenState();
+}
+
+class _AddWalletScreenState extends State<AddWalletScreen> {
+  int selectedValue = 0;
+
+  final List<String> items = [
+    '35',
+    '100',
+    '25',
+    '45',
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -39,10 +53,49 @@ class AddWalletScreen extends StatelessWidget {
             const SizedBox(
               height: 20,
             ),
+            SizedBox(
+              height: 100,
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: items.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25),
+                      child: InkWell(
+                        onTap: () {
+                          setState(() {
+                            selectedValue = index;
+                          });
+                        },
+                        child: Container(
+                          width: 40,
+                          height: 40,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color:
+                                  selectedValue == index ? Colors.green : null),
+                          child: Center(
+                            child: Text(
+                              items[index],
+                              style: TextStyle(
+                                  color: selectedValue == index
+                                      ? Colors.white
+                                      : null),
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  }),
+            ),
+            const Spacer(),
             FloatingActionButton.extended(
               onPressed: () {},
               label: const Icon(Icons.add),
-            )
+            ),
+            const SizedBox(
+              height: 50,
+            ),
           ],
         ),
       ),
